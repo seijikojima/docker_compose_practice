9457a9af7542f9c5412989b6fa26aaffaf2f6c0c
sabosan <sabosan331@gmail.com>
2020-10-28 10:05:59 +0900

    complete dockerizing vue, not complete node, mongodb

---
+++ vue/src/App.vue
@@ -0,0 +1,45 @@
+
+<template>
+
+  <div id="app">
+    <!-- <Header /> -->
+    <Todos />
+    
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'app',
+  components: {
+    Todos,
+  },
+}
+</script>
+
+<style>
+* {
+  box-sizing : border-box;
+  margin : 0;
+  padding : 0;
+}
+body {
+    font-family: Arial,Helvetica,sans-serif;
+    line-height: 1.4;
+}
+.btn{
+    display: inline-block;
+    border:none;
+    background:#D54E21;
+    color:#fff;
+    padding: 7px 20px;
+    cursor:pointer;
+}
+.btn:hover {
+  background : rgb(213, 111, 33);
+}
+
+
+</style>
